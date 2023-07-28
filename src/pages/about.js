01781c407103470016c1a2137ac2b0ccf5a2cc23
Weltton Alves Ortega <welttonao@gmail.com>
2023-07-28 13:59:44 -0400

    Fix: Replace jpg to png

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,7 +2,7 @@ import { Container, Heading, Stack, chakra, Text } from '@chakra-ui/react'
 import Image from 'next/image'
 
 import { FaFile, FaLinkedin } from 'react-icons/fa'
-import AvatarProfile from '../../public/images/aboutphoto.jpg'
+import AvatarProfile from '../../public/images/aboutphoto.png'
 
 
 const SocialButton = ({
