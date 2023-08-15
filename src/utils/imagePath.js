356234db646ffb5d38bdfada9228f931464d040e
Weltton Alves Ortega <welttonao@gmail.com>
2023-08-14 21:11:42 -0400

    Update: change illustrations image path

--- src/utils/imagePath.js
+++ src/utils/imagePath.js
@@ -1,41 +1,41 @@
 export const images = [
     {
-        src: '../images/illustrations/WilderMind.png',
+        src: 'https://live.staticflickr.com/65535/53117475300_af1eb3ca5d_o.png',
         thumbnail: '../images/thumbnail/WilderMind-thumbnail.webp',
         title: 'Wilder Mind',
         creationDate: '2022',
         alt: 'Wilder Mind',
     },
     {
-        src: '../images/illustrations/Fenêtres.png',
+        src: 'https://live.staticflickr.com/65535/53117561758_49faae15f4_o.png',
         thumbnail: '../images/thumbnail/Fenetres-thumbnail.webp',
         title: 'Fenêtres',
         creationDate: '2022',
         alt: 'Fenêtres',
     },
     {
-        src: '../images/illustrations/Submergé.png',
+        src: 'https://live.staticflickr.com/65535/53117068321_54f31c331f_o.png',
         thumbnail: '../images/thumbnail/Submerge-thumbnail.webp',
         title: 'Submergé',
         creationDate: '2022',
         alt: 'Submergé',
     },
     {
-        src: '../images/illustrations/Cherry.png',
+        src: 'https://live.staticflickr.com/65535/53117566103_381b36b6d9_o.png',
         thumbnail: '../images/thumbnail/Cherry-thumbnail.webp',
         title: 'Cherry',
         creationDate: '2022',
         alt: 'Cherry ',
     },
     {
-        src: '../images/illustrations/Kaifuku.png',
+        src: 'https://live.staticflickr.com/65535/53116470437_3a05f3b4dc_o.png',
         thumbnail: '../images/thumbnail/Kaifuku-thumbnail.webp',
         title: 'Kaifuku',
         creationDate: '2022',
         alt: 'Kaifuku',
     },
     {
-        src: '../images/illustrations/Farewell.png',
+        src: 'https://live.staticflickr.com/65535/53117475480_67cfb93c4e_o.png',
         thumbnail: '../images/thumbnail/Farewell-thumbnail.webp',
         title: 'Farewell',
         creationDate: '2022',
@@ -43,7 +43,7 @@ export const images = [
     },
 
     {
-        src: '../images/illustrations/Haru.png',
+        src: 'https://live.staticflickr.com/65535/53117271729_224192e1c4_o.png',
         thumbnail: '../images/thumbnail/Haru-thumbnail.webp',
         title: 'Haru',
         creationDate: '2022',
@@ -51,7 +51,7 @@ export const images = [
     },
 
     {
-        src: '../images/illustrations/NouveauDepart.png',
+        src: 'https://live.staticflickr.com/65535/53117271719_094ae62777_o.png',
         thumbnail: '../images/thumbnail/NouveauDepart-thumbnail.webp',
         title: 'Nouveau Depart',
         creationDate: '2022',
@@ -59,7 +59,7 @@ export const images = [
     },
 
     {
-        src: '../images/illustrations/Refuge.png',
+        src: 'https://live.staticflickr.com/65535/53117271694_720097b0a2_o.png',
         thumbnail: '../images/thumbnail/Refuge-thumbnail.webp',
         title: 'Refuge',
         creationDate: '2022',
